afa8132788ef8cfc49457a98a97c94666009db32
Bas Terwijn <B.Terwijn@uva.nl>
2013-07-15 14:44:03 +0200

    added scripts to play Heerlen recordings

---
+++ accompany_uva/scripts/downloadHeerlen.sh
@@ -0,0 +1,15 @@
+
+cd ../
+mkdir -p res
+cd res
+mkdir -p Heerlen
+cd Heerlen
+
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/2013-06-14-13-40-37.bag
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/2013-06-14-13-45-44.bag
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/2013-06-14-13-50-10.bag
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/2013-06-14-13-50-31.bag
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/2013-06-14-13-59-27.bag
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/2013-06-14-14-04-42.bag
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/record.sh
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/recording_info.txt
