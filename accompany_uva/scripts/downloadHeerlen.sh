155bd911a765bcb4b2220352b7fc0bf839e0fb7c
Bas Terwijn <B.Terwijn@uva.nl>
2013-07-18 11:54:30 +0200

    files to download play and view Heerlen datasets are working

--- accompany_uva/scripts/downloadHeerlen.sh
+++ accompany_uva/scripts/downloadHeerlen.sh
@@ -11,5 +11,15 @@ wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/2013-06-14-13-50-10
 wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/2013-06-14-13-50-31.bag
 wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/2013-06-14-13-59-27.bag
 wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/2013-06-14-14-04-42.bag
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/entryExit.txt
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/frame.dat
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/map.pgm
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/map.yaml
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/params.xml
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/prior.txt
 wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/record.sh
 wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/recording_info.txt
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/camera_extrinsic_kitchen.xml
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/camera_extrinsic_sofa.xml
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/camera_intrinsic_kitchen.xml
+wget -nc http://basterwijn.nl/bterwijn/Accompany/HeerlenData/camera_intrinsic_sofa.xml
